0187854ec07926713b5c70f6d26ba2f89aec976d
PhanDuc2k3 <phanminhduc2003@gmail.com>
2024-04-16 14:00:10 +0700

    commit code

---
+++ src/main.java
@@ -0,0 +1,13 @@
+public class main {
+
+    public static void main(String[] args) {
+        int number = 10;
+        if (number > 0) {
+            for (int i = 0; i < number; i++) {
+                System.out.println("Count: " + i);
+            }
+        } else {
+            System.out.println("Number is not positive.");
+        }
+    }
+}
